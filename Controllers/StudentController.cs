f474a62542bb56163811b77a8d12417cc216f125
devidsmith9527-crypto <soeurnglim@gmail.com>
2026-07-01 19:12:19 +0700

    Update STudent

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -15,12 +15,12 @@ namespace OopMvcApplication.Controllers
         [HttpGet("all")]
         public IActionResult Index() {
             List<Student> students = new() {
-                new(1, "Dara SOK", "Computer Science"), 
-                new(2, "Cheata SENG", "Information Technology"),
-                new(3, "Sopheap CHHAY", "Software Engineering"),
-                new(4, "Sokha CHHAY", "Computer Science"),
-                new(5, "Sophal CHHAY", "Information Technology"),
-                new(6, "Sophal CHHAY", "Software Engineering")
+                new(1, "Dara SOK", "Computer Science", "dara_sok", new DateTime(2000, 1, 1), "dara.sok@example.com"),
+                new(2, "Cheata SENG", "Information Technology", "cheata_seng", new DateTime(2000, 2, 2), "cheata.seng@example.com"),
+                new(3, "Sopheap CHHAY", "Software Engineering", "sopheap_chhay", new DateTime(2000, 3, 3), "sopheap.chhay@example.com"),
+                new(4, "Sokha CHHAY", "Computer Science", "sokha_chhay", new DateTime(2000, 4, 4), "sokha.chhay@example.com"),
+                new(5, "Sophal CHHAY", "Information Technology", "sophal_chhay", new DateTime(2000, 5, 5), "sophal.chhay@example.com"),
+                new(6, "Sophal CHHAY", "Software Engineering", "sophal_chhay", new DateTime(2000, 6, 6), "sophal.chhay@example.com")
             };
             return View(students);
         }
--- Models/Student.cs
+++ Models/Student.cs
@@ -10,11 +10,18 @@ namespace OopMvcApplication.Models
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Major { get; set; } = string.Empty;
-        public Student(int id, string name, string major) 
+        public string Telegram { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; } = DateTime.Now;
+        public string Email { get; set; } = string.Empty;
+        public Student(int id, string name, string major, string telegram = "", DateTime dateOfBirth = default, string email = "") 
         { 
+            //Property = Parameter
             Id = id; 
             Name = name; 
             Major = major; 
+            Telegram = telegram;
+            DateOfBirth = dateOfBirth;
+            Email = email;
         }
     }
 }
